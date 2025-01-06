5c4014a3843afbb838239b0ae0ee1feefe7f7470
StefandePutter <Stefandeputter@gmail.com>
2025-01-06 11:02:25 +0100

    yes

---
+++ next-g3n/src/app/components/product.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import productStyles from "./product.module.css";
+import { useEffect, useState } from "react";
+
+export function Product(product) {
+    product = product.product;
+	console.log(product);
+
+	return (
+		<table>
+			<tbody className={productStyles.table}>
+				<tr>
+					<th>Id</th>
+					<th>Title</th>
+					<th>Price</th>
+					<th>Stock</th>
+				</tr>
+				<tr>
+					<td>{product.id}</td>
+					<td>{product.title}</td>
+					{
+						(product.price % 1 == 0) ? (
+
+							<td>{"€"+product.price+",-"}</td>
+						) : (
+							<td>{"€"+(product.price)}</td>
+						)
+					}
+					<td>{parseInt(product.isSold) ? "Sold" : "In supply"}</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+}
